38b9db50716c48b10c1e221f59e428316aec9133
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2023-10-12 18:52:02 +0600

    [DONE] jwt helpers added

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,4 +7,6 @@ export default {
   ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL as string,
+  JWT_SECRET_ACCESS: process.env.JWT_SECRET_ACCESS,
+  JWT_SECRET_EXPIRY: process.env.JWT_SECRET_EXPIRY,
 };
